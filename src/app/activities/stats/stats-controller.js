455b384e0e8136d864e1bda4ea7a28bf5ecac6d3
felipefujioka <felipefujioka@gmail.com>
2016-07-16 14:46:54 -0300

    user info

--- src/app/activities/stats/stats-controller.js
+++ src/app/activities/stats/stats-controller.js
@@ -9,5 +9,24 @@ angular.module('zoominStats')
     '_',
     function($scope,$stateParams, $cookies,zoominHTTP,_){
       var token = $cookies.get('zoominFbToken');
-      
+      var http = new zoominHTTP();
+      var buildFields = function() {
+        return ['name,picture,birthday'];
+      };
+
+      var requestUserData = function(page){
+        var self = this;
+        return http.get('https://graph.facebook.com/v2.6','/me',{access_token:token, fields:buildFields(), limit:49})
+        .then(function(response){
+          var data = response.data;
+          $scope.user = {
+            name: data.name,
+            birthday: data.birthday,
+            pictureUrl: data.picture.data.url
+          };
+        });
+      };
+
+      requestUserData();
+
     }]);
